b478b88789315c256a321f883b094770cf34393d
sundensky <sundensky@163.com>
2019-06-28 15:31:51 +0800

    chapter6 end
    第六章代码编写完成并运行访问成功，第七章刚刚开始。

--- blogsystem/comment/admin.py
+++ blogsystem/comment/admin.py
@@ -1,3 +1,12 @@
+# coding=UTF-8
 from django.contrib import admin
 
+from .models import Comment
+
+from blogsystem.base_admin import BaseOwnerAdmin
+
+
 # Register your models here.
+@admin.register(Comment)
+class CommentAdmin(BaseOwnerAdmin):
+    list_display = ('target', 'nickname', 'content', 'website', 'created_time')

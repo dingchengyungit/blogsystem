fe7379dde94a5587d3b010e29d47d25bf9cf8686
sundensky <sundensky@163.com>
2019-07-04 19:25:42 +0800

    增加测试装饰器代码

---
+++ blogsystem/blogsystem/tests/testdecoration.py
@@ -0,0 +1,20 @@
+import time
+import requests
+
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print(func.__name__, 'cost', time.time() - start)
+        return result
+
+    return wrapper
+
+
+@time_it
+def fetch_page():
+    requests.get('http://www.baidu.com')
+
+
+fetch_page()

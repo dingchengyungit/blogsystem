40c88cef28af8f1c3072198d993f3f07539fc639
sundensky <sundensky@163.com>
2019-07-05 09:15:53 +0800

    12章结束
    12章主要是讲了调试和优化，尤其是debug-toolbar和silk两个工具非常实用！对于初学者来说，知道最重要！
    toolbar适用于个人，也就是开发环境，silk适用于测试环境。
    调优思路：减少外部I、O，减少冗余调用/sql查询，优化耗时逻辑

--- blogsystem/blogsystem/settings/develop.py
+++ blogsystem/blogsystem/settings/develop.py
@@ -14,17 +14,27 @@ DATABASES = {
 }
 
 INSTALLED_APPS += [
-    'debug_toolbar',
+    # 'debug_toolbar',
+    # 'pympler',
+    # 'debug_toolbar_line_profiler', 安装失败
+    'silk'
 ]
 
 MIDDLEWARE += [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'silk.middleware.SilkyMiddleware'
 ]
 
 INTERNAL_IPS = ['127.0.0.1']
 
 
-# djdt_flamegraph火焰图，由于信号的系统不兼容，windows下无法正常使用，暂时关闭
+# DEBUG_TOOLBAR_CONFIG = {
+#     'JQUERY_URL': ' https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js',
+# }
+
 # DEBUG_TOOLBAR_PANELS = [
-#     'djdt_flamegraph.FlamegraphPanel',
+    # djdt_flamegraph火焰图，由于信号的系统不兼容，windows下无法正常使用，暂时关闭
+    #     'djdt_flamegraph.FlamegraphPanel',
+    # 'pympler.panels.MemoryPanel',  # 运行报错
+    # 'debug_toolbar_line_profiler.panel.ProfilingPanel',
 # ]
